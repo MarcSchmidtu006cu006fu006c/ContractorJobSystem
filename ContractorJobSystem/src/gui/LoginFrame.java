1e592e39d55a3b27f2bc1b24d5aad15f3c0b994b
MarcSchmidt <marcemarc101@gmail.com>
2022-08-04 16:26:41 -0600

    - Changed dimension of JFrame to 900 x 600 px - Login frame title changed to Contractor Job System - JLabel added to login Frame with “Please Login” in size 32 font - JLabel added to login Frame with “If you do not have an account please click request account” in size 13 font - JLabel added to login Fram with “Username: “ in size 16 font - JTextField added to login frame to get username from user - JLabel and JTextField added with same dimensions for password - JButton added to login frame for user click login - JButton added to login frame for user to request access

--- ContractorJobSystem/src/gui/LoginFrame.java
+++ ContractorJobSystem/src/gui/LoginFrame.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class LoginFrame extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -31,12 +40,57 @@ public class LoginFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginFrame() {
+		setTitle("Contractor Job System");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 900, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Please Login");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 32));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(6, 6, 890, 38);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("If you do not have an account please click \"Request Access\"");
+		lblNewLabel_1.setBounds(268, 56, 382, 16);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Username: ");
+		lblNewLabel_2.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setBounds(245, 146, 94, 23);
+		contentPane.add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		lblNewLabel_2.setLabelFor(textField);
+		textField.setBounds(332, 144, 244, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Password: ");
+		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2_1.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblNewLabel_2_1.setBounds(245, 181, 94, 23);
+		contentPane.add(lblNewLabel_2_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(332, 181, 244, 26);
+		contentPane.add(textField_1);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(385, 219, 117, 29);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Request Access");
+		btnNewButton_1.setBounds(377, 288, 147, 23);
+		contentPane.add(btnNewButton_1);
 	}
-
 }
